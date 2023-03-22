026c50f1bf8d4cae7de1f00dc8ec31bb09862dba
River Hansen <riverbh01@gmail.com>
2023-03-22 15:14:54 -0600

    changed the color

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('button')
 
-const clickHandler = () => alert('The cat name is: Rascal')
+const clickHandler = () => alert('The cat name is: Charlie')
 
 btn.addEventListener('click', clickHandler)
