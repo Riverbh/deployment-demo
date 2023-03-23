1be2d4a46cfe36a9e3790cd788f8d9fe81a50ee3
River Hansen <riverbh01@gmail.com>
2023-03-23 15:51:46 -0600

    idek

--- server.js
+++ server.js
@@ -27,8 +27,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/api/students', (req, res) => {
-    //I want to get an info if students name are sent
-    rollbar.info('The Students name werent sent')
+
     res.status(200).send(students)
 })
 
@@ -42,22 +41,18 @@ app.post('/api/students', (req, res) => {
    try {
        if (index === -1 && name !== '') {
            students.push(name)
-           //I want to get an rollbar info when a new student name is added
-           rollbar.info(`New student name is added: ${name}`)
+          
            res.status(200).send(students)
        } else if (name === ''){
-        //Want to get a warning when theres no inpu
-            rollbar.warning('there was no name entered')
+        
            res.status(400).send('You must enter a name.')
        } else {
-        //want a warning when someone enters an already existing name
-            rollbar.warning('someone entered an exisiting name')
+        
            res.status(400).send('That student already exists.')
        } 
    } catch (err) {
        console.log(err)
-    //I want to get an error message to rollbar
-    rollbar.error(err)
+    
    }
 })
 
@@ -65,8 +60,7 @@ app.delete('/api/students/:index', (req, res) => {
     const targetIndex = +req.params.index
 
    let deleteNmae = students.splice(targetIndex, 1)
-    //get an info when a student is deleted 
-    rollbar.info(`someone deleted the name ${deleteNmae}`)
+   
     res.status(200).send(students)
 })
 
